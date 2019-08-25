1b7b5088c7ac97b298fc99f21f118704986276ca
Jonathan Prieto <jonathan.prieto@me.com>
2019-08-25 10:45:14 -0500

    Ok

---
+++ cmake-build-debug/CMakeFiles/tex-quotes-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tex-quotes-jonathanprieto99.dir/main2.cpp.o"
+  "tex-quotes-jonathanprieto99.pdb"
+  "tex-quotes-jonathanprieto99"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tex-quotes-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
